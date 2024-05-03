a9a1f6802e6d9542457342c2c8c609bdbbaccbf1
Uttam-001 <ycuttam@gmail.com>
2024-05-03 14:00:10 +0000

    datatypes

---
+++ 01_basics/02_dataTypes.js
@@ -0,0 +1,22 @@
+"use strict"; //treat all JS code as newer version
+
+// alert(3+3) // we are using node JS, not browser
+
+ let name = "bunny"
+ let age = 18
+ let isLoggedIn = false
+ let state = null
+
+ // number => 2 to power 53
+ // bigint
+ // string=> ""
+ // boolean => true/false
+ // null => standalone value
+ // undefined
+ // symbol => unique
+
+
+ // object
+
+console.log(typeof undefined)
+console.log(typeof null)
